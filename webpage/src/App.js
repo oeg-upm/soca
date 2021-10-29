5668e15f8ea497ac29e7ccc60644bb17a75d794c
Esteban Gonzalez Guardia <egonzalez@ciclope.info>
2021-10-29 05:18:29 +0200

    Filter component added

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 //import { BasicTable } from './components/BasicTable'
-import { FilteringTable } from './components/FilteringTable'
+//import { FilteringTable } from './components/FilteringTable'
 //import { PaginationTable } from './components/PaginationTable'
+import { CatalogTable } from './components/CatalogTable'
 
 function App() {
   return (
     <div className="App">
-      <FilteringTable />
+      <CatalogTable />
     </div>
   );
 }
