517204e0e5246de9b64f8d81f0b0df25e9f156d1
Daniel Rodriguez Mariblanca <dakixr@gmail.com>
2022-01-25 17:52:28 +0100

    New card design implementation

--- commands/software_catalog_portal.py
+++ commands/software_catalog_portal.py
@@ -18,6 +18,12 @@ def generate(input, output):
     # Copy all img to destination folder
     copy_tree(f"{base}/assets/img", f"{output}/img")
 
+    # Copy all languague_icons
+    copy_tree(f"{base}/assets/language_icons", f"{output}/language_icons")
+
+    # Copy all repo_icons
+    copy_tree(f"{base}/assets/repo_icons", f"{output}/repo_icons")
+
     # load the template
     with open(f"{base}/assets/template.html") as template:
         soup = BeautifulSoup(template.read(), features="html.parser")
@@ -68,24 +74,51 @@ def card_view(metadata):
     description = description[:200]+'...' if len(description[:200]) == 200 else description
 
     html_card = f"""
-      <article class="item">
-	  <h4 class="title">{title}</h4>
-	  <img src="img/summico.png" class="ico">
-	  <div class="description">
-	  	<p>{description}</p>
-	  </div>
-   	<div class="bottom">      
-      <a href="lynx-swagger-ui.html?openapi-server-url=https://apis.lynx-project.eu/doc/open-api-3/entity-linking"><img src="img/openapi.png" width="24" alt="API description"></a>
-      <a href="https://www.poolparty.biz/"><img src="img/home.png" width="24" alt="Webpage"></a>
-      <a href="https://zenodo.org/record/3558282#"><img src="img/pdf24.png" width="24" alt="Deliverable"></a>
-		  <a href="https://www.youtube.com/watch?v=hDgk65GuORc"><img src="img/youtube.png" width="24" alt="Video"></a>        
-    </div>
-    <div class="bottom" style="right:10px; float:right;">
-      <small>
-        <kbd title="English">EN</kbd><kbd title="Spanish">ES</kbd><br>
-        <kbd title="German">DE</kbd><kbd title="Dutch">NL</kbd>
-      </small>
-    </div> 
-  </article>"""
+    <article class="card">
+
+        <div class="card-row">
+            <div class="card-col1">
+                <h4 class="title">{title}</h4>
+                <p class="description">{description}</p>
+            </div>
+            <div class="card-col2">
+                <img src="img/github-default.svg" alt="repo-logo" class="repo-logo">
+                <div class="recently-updated"></div>
+                <div class="flex-horizontal float-right" style="margin-top: 0.3rem;">
+                    <p><b>99</b></p>
+                    <img src="repo_icons/star.png" alt="stars" class="repo-icon">
+                </div>
+                <div class="flex-horizontal float-right">
+                    <p><b>10.2k</b></p>
+                    <img src="repo_icons/watching.png" alt="watching" class="repo-icon">
+                </div>
+            </div>
+        </div>
+
+        <div class="card-row">
+            <div class="card-col1">
+                <div class="flex-horizontal">
+                    <img src="repo_icons/license.png" alt="license" class="repo-icon">
+                    <img src="repo_icons/notebook.png" alt="notebook" class="repo-icon">
+                    <img src="repo_icons/readme.png" alt="readme" class="repo-icon">
+                    <img src="repo_icons/download.png" alt="stars" class="repo-icon">
+                    <img src="repo_icons/citation.png" alt="citation" class="repo-icon">
+                    <img src="repo_icons/docker.png" alt="docker" class="repo-icon">
+                    <img src="repo_icons/paper.png" alt="paper" class="repo-icon">
+                    <img src="repo_icons/installation.png" alt="installation" class="repo-icon">
+                    <img src="repo_icons/requirements.png" alt="requirements" class="repo-icon">
+                </div>
+            </div>
+            <div class="card-col2">
+                <div class="flex-horizontal float-right grey-color-svg">
+                    <img src="language_icons/html.svg" alt="html" class="repo-icon">
+                    <img src="language_icons/python.svg" alt="python" class="repo-icon">
+
+                </div>
+            </div>
+        </div>
+    
+    </article>
+"""
 
     return html_card
