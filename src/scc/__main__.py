76b7f8029f378150ccbb8868f446d98cfb7c71c5
Daniel Rodriguez Mariblanca <dakixr@gmail.com>
2022-01-28 15:13:55 +0100

    pypackage setup

--- src/scc/__main__.py
+++ src/scc/__main__.py
@@ -1,5 +1,4 @@
 import click
-from commands import organization_repositories, repositories_metadata, software_catalog_portal
 
 @click.group()
 def cli():
@@ -20,6 +19,7 @@ def cli():
 @click.option('--output','-o', default="repos.csv", help="Output data will be stored in a new csv file.")
 def repos(input, output):
     """Retreive all organization/s repositories."""
+    from .commands import organization_repositories
     organization_repositories.fetch(input, output)
 
 @cli.command()
@@ -27,6 +27,7 @@ def repos(input, output):
 @click.option('--output','-o', default="repos-metadata", help="Output dir where repositories metadata will be saved.")
 def extract(input, output):
     """Execute  SOMEF for  every  repo introduced."""
+    from .commands import repositories_metadata
     repositories_metadata.fetch(input, output)
 
 @cli.command()
@@ -34,7 +35,8 @@ def extract(input, output):
 @click.option('--output','-o', default="portal", help="Outout dir where the Software Catalog Portal will be saved.")
 def portal(input, output):
     """Build  a  portal  with a  minimalistic desing."""
+    from .commands import software_catalog_portal
     software_catalog_portal.generate(input, output)
 
-if __name__ == "__main__":
-    cli()
+#if __name__ == "__main__":
+#    cli()
