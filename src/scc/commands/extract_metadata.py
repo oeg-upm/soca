ef0c1fe31986c2e24694d1f366cd55f36b662dd5
Daniel Rodriguez Mariblanca <dakixr@gmail.com>
2022-02-22 23:55:14 +0100

    new extracted data

--- src/scc/commands/extract_metadata.py
+++ src/scc/commands/extract_metadata.py
@@ -32,11 +32,12 @@ def fetch(repos_csv, output):
         repos_url = str(repos_csv).split(",")
 
     print("It may take a while... Depends on repository size and Github API limitations.")
+    failed_repos = []
     for repo_url in progressbar(repos_url, redirect_stdout=True):
         try:
             print(f"Extracting metadata from {repo_url}")
-            #with HiddenPrints():
-            metadata = cli_get_data(0.9, False, repo_url)
+            with HiddenPrints():
+                metadata = cli_get_data(0.9, False, repo_url)
             repo_full_name = (repo_url[19:]).replace("/", "_").replace(".","-")
             with open(f"{output}/{repo_full_name}.json", 'w') as repo_metadata:
                 json.dump(metadata, repo_metadata, indent = 4)
@@ -46,5 +47,12 @@ def fetch(repos_csv, output):
               
         except:
             print(f"ERROR: Could not extract metadata from {repo_url}")
+            failed_repos.append(repo_url)
     
+    if len(failed_repos) > 0:
+        print("ERROR: metadata could not be extracted from the following repo/s:")
+        for fr in failed_repos:
+            print(fr)
+    
+    print(f"\nSuccessfully extracted metadata from ({len(repos_url)-len(failed_repos)}/{len(repos_url)}) repositories.")
 
