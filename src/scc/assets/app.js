7db7845c50651d7515d130b5c1880050d051a15f
Daniel Rodriguez Mariblanca <dakixr@gmail.com>
2022-04-08 12:26:52 +0200

    swipe gestures

--- src/scc/assets/app.js
+++ src/scc/assets/app.js
@@ -398,4 +398,37 @@ function checkKey(e) {
 
 }
 
+// Change page with swipe gestures for mobiles devices
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const gestureZone = document;
+
+gestureZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+gestureZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false); 
+
+function handleGesture() {
+    if (touchendX+140 <= touchstartX) {
+        console.log('Swiped left');
+        current_page++;
+        search();
+    }
+    
+    if (touchendX-140 >= touchstartX) {
+        console.log('Swiped right');
+        current_page--;
+        search();
+    }
+}
+
 loadCardsData();
