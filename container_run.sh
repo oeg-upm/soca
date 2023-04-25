3f40424947d666f7ada337c5e9edec6ee2a647aa
Str3am786 <arroyo.m786@gmail.com>
2023-04-25 17:29:02 +0200

    added inspect 4 py to the container_run.sh

--- container_run.sh
+++ container_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
 soca fetch -i oeg-upm --org -o oeg-upm_repos.csv --not_archived  #modify -i ______ to organisation, add --user if its a user
-soca extract -i oeg-upm_repos.csv -o oeg-upm_metadata
+soca extract -i oeg-upm_repos.csv -o oeg-upm_metadata -i4p
 soca portal -i oeg-upm_metadata
 soca summary -i portal/cards_data.json -U
